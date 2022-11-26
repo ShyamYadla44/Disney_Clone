a7eaeb07ef9c9b9ff5220f315099112a0180bcc2
Yadla Syam Sundar <106598369+ShyamYadla44@users.noreply.github.com>
2022-11-26 14:42:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+// // sliders
+// let slideContainers=[...document.querySelector('.slider')];
+// let prevButtons=[...document.querySelector('.prev-button')];
+// let nextButtons=[...document.querySelector('.next-button')];
+
+// slideContainers.forEach((item,i)=>{
+//     let containerDimensions=item.getBoundingClientRect();
+//     let containerWidth=containerDimensions.width;
+
+//     nextButtons[i].addEventListener('click',()=>{
+//         item.scrollLeft += containerWidth - 200;
+//     })
+
+//     prevButtons[i].addEventListener('click',()=>{
+//         item.scrollLeft -= containerWidth +200;
+//     })
+// });
+
+
+// Video Cards
 let cardContainers=[...document.querySelectorAll('.card-container')];
 let preButtons=[...document.querySelectorAll('.pre-button')];
-let nextButtons=[...document.querySelectorAll('.nxt-button')];
+let nxtButtons=[...document.querySelectorAll('.nxt-button')];
 
 cardContainers.forEach((item,i)=>{
     let containerDimensions=item.getBoundingClientRect();
     let containerWidth=containerDimensions.width;
 
-    nextButtons[i].addEventListener('click',()=>{
+    nxtButtons[i].addEventListener('click',()=>{
         item.scrollLeft += containerWidth - 200;
     })
 
